8a3c0a68c573f25ee133411e919bca2dd5f1609b
Lari Saukkonen <lari.saukkonen@gmail.com>
2025-09-28 20:51:56 +0300

    init: ensimmäinen versio

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HallwayTenantManager from './HallwayTenantManager'
+
+export default function App() {
+  return (
+    <div className="min-h-screen p-4">
+      <HallwayTenantManager />
+    </div>
+  )
+}
