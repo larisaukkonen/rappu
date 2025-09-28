fe2befc02af822271bc82437f0c86618d626361d
Lari Saukkonen <lari.saukkonen@gmail.com>
2025-09-28 20:49:01 +0300

    chore: untrack node_modules

--- src/components/ui/input.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { cn } from '@/lib/utils'
-
-export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, ...props }, ref) => (
-    <input ref={ref} className={cn('h-9 w-full rounded-md border border-neutral-300 px-3 outline-none focus:ring-2 focus:ring-neutral-300', className)} {...props} />
-  )
-)
-Input.displayName = 'Input'
