72004ee1361d14c4fef7109847c81afe04a11976
Lari Saukkonen <lari.saukkonen@gmail.com>
2025-09-28 21:36:20 +0300

    chore: add api/hello sanity route

---
+++ api/hello.ts
@@ -0,0 +1,4 @@
+﻿import type { VercelRequest, VercelResponse } from "@vercel/node";
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({ ok: true, msg: "api up" });
+}
